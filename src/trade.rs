2136f91457c0e41a0a14e4b294a3bbf5f881917e
Ruan Petterson <ruan.petterson@ripio.com>
2022-07-14 15:05:21 -0300

    feat: added `TryInto` trait for `Trade` from `Asset`

--- src/trade.rs
+++ src/trade.rs
@@ -1,7 +1,16 @@
+use crate::{Asset, Order};
+
 use super::OrderId;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum TradeError {
+    #[error("taker and maker must be opposite each other")]
+    MismatchSides,
+}
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(Copy, Clone))]
@@ -11,4 +20,15 @@ pub struct Trade {
     pub(super) maker: OrderId,
     pub(super) amount: u64,
     pub(super) price: u64,
+}
+
+impl<Order: Asset<Trade = Self>> TryFrom<(&mut Order, &mut Order)> for Trade {
+    type Error = TradeError;
+
+    #[inline]
+    fn try_from(
+        (taker, maker): (&mut Order, &mut Order),
+    ) -> Result<Self, Self::Error> {
+        taker.trade(maker).ok_or(TradeError::MismatchSides)
+    }
 }
